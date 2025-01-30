51eae1b71566a9bd57241f828519f52adf9de0de
Melody <99524525+M10327@users.noreply.github.com>
2025-01-30 05:28:38 -0600

    add missing variable

--- js/stylemanager.js
+++ js/stylemanager.js
@@ -1,9 +1,8 @@
-
+var styleKey = "1999";
 function SetStyle(style, reshuffle){
     styleKey = style;
     setCookie("style", styleKey, 60);
     var el = document.getElementById("themeUrl");
     el.href = "css/theme_"+ styleKey + ".css";
     if (reshuffle == true) OpenWindow("settingswindow", false);
-    console.log("set style to " + styleKey);
 }
